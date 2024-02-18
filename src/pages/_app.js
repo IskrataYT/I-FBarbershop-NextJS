42c42120f943595a743b6d1d0c2020b47baacd3e
Iskren Minkov <iskratayt1231@gmail.com>
2024-02-18 02:50:52 +0200

    Changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css" // path to your globals.css
+import { Analytics } from "@vercel/analytics/react" // import the Analytics component
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
+
