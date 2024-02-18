d8774aaef6f07713d1aa717c12d5376cc2a9fbd1
Iskren Minkov <iskratayt1231@gmail.com>
2024-02-18 02:55:08 +0200

    added speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css" // path to your globals.css
 import { Analytics } from "@vercel/analytics/react" // import the Analytics component
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
