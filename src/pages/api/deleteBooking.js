a4219970638a9992384ca3f0b9056f5985f730bd
Iskren Minkov <iskratayt1231@gmail.com>
2024-02-19 20:19:13 +0200

    asdasd

--- src/pages/api/deleteBooking.js
+++ src/pages/api/deleteBooking.js
@@ -1,29 +1,29 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb")
 
 async function deleteBooking(date, service, time) {
-  const url = 'mongodb+srv://root:EHp3wLINM3dPT6x4@db1.yr4dzh4.mongodb.net/?retryWrites=true&w=majority';
-  const dbName = 'myproject';
-  const client = new MongoClient(url);
+  const url = "mongodb+srv://root:EHp3wLINM3dPT6x4@db1.yr4dzh4.mongodb.net/?retryWrites=true&w=majority"
+  const dbName = "myproject"
+  const client = new MongoClient(url)
 
   try {
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection('bookings');
+    await client.connect()
+    const db = client.db(dbName)
+    const collection = db.collection("bookings")
     console.log("Date: " + date + ", Service: " + service + ", Time: " + time)
     // Delete the booking
-    const result = await collection.deleteOne({ date: date, service: service, time: time });
+    const result = await collection.deleteOne({ date: date, service: service, time: time })
 
     if (result.deletedCount === 1) {
-      console.log(`Successfully deleted one document.`);
+      console.log("Successfully deleted one document.")
     } else {
-      console.log(`No documents matched the query. Deleted 0 documents.`);
+      console.log("No documents matched the query. Deleted 0 documents.")
     }
   } catch (err) {
-    console.log(err.stack);
-    throw err;
+    console.log(err.stack)
+    throw err
   } finally {
-    client.close();
+    client.close()
   }
 }
 
-module.exports = deleteBooking;
+module.exports = deleteBooking
