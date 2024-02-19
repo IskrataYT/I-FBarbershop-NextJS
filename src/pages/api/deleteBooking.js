0e17607a63bf3de8f4e4a707c171389dd9dc56d5
Iskren Minkov <iskratayt1231@gmail.com>
2024-02-19 21:57:06 +0200

    linted

--- src/pages/api/deleteBooking.js
+++ src/pages/api/deleteBooking.js
@@ -1,32 +1,32 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb"
 
 export default async function handler(req, res) {
-  const { date, service, time } = req.body;
+  const { date, service, time } = req.body
 
-  const url = process.env.MONGODB_URL;
-  const dbName = process.env.DB_NAME;
-  const client = new MongoClient(url);
+  const url = process.env.MONGODB_URL
+  const dbName = process.env.DB_NAME
+  const client = new MongoClient(url)
 
   try {
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection('bookings');
-    console.log(`Date: ${date}, Service: ${service}, Time: ${time}`);
+    await client.connect()
+    const db = client.db(dbName)
+    const collection = db.collection("bookings")
+    console.log(`Date: ${date}, Service: ${service}, Time: ${time}`)
 
     // Delete the booking
-    const result = await collection.deleteOne({ date, service, time });
+    const result = await collection.deleteOne({ date, service, time })
 
     if (result.deletedCount === 1) {
-      console.log('Successfully deleted one document.');
-      res.status(200).json({ message: 'Successfully deleted one document.' });
+      console.log("Successfully deleted one document.")
+      res.status(200).json({ message: "Successfully deleted one document." })
     } else {
-      console.log('No documents matched the query. Deleted 0 documents.');
-      res.status(404).json({ message: 'No documents matched the query. Deleted 0 documents.' });
+      console.log("No documents matched the query. Deleted 0 documents.")
+      res.status(404).json({ message: "No documents matched the query. Deleted 0 documents." })
     }
   } catch (err) {
-    console.log(err.stack);
-    res.status(500).json({ message: 'An error occurred.', error: err });
+    console.log(err.stack)
+    res.status(500).json({ message: "An error occurred.", error: err })
   } finally {
-    client.close();
+    client.close()
   }
 }
