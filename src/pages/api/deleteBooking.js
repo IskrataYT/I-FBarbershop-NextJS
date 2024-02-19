e8c4972720d433333777df62075ac9bea7691f12
Iskren Minkov <iskratayt1231@gmail.com>
2024-02-19 22:02:19 +0200

    added reverse translation for the deleteBooking

--- src/pages/api/deleteBooking.js
+++ src/pages/api/deleteBooking.js
@@ -1,32 +1,34 @@
 import { MongoClient } from "mongodb"
+import serviceTranslations from "./models/serviceTranslations"
 
-export default async function handler(req, res) {
-  const { date, service, time } = req.body
-
+async function deleteBooking(date, service, time, language = "bg") {
   const url = process.env.MONGODB_URL
   const dbName = process.env.DB_NAME
   const client = new MongoClient(url)
+
+  // Reverse the translation
+  const serviceKey = Object.keys(serviceTranslations[language]).find(key => serviceTranslations[language][key] === service)
 
   try {
     await client.connect()
     const db = client.db(dbName)
     const collection = db.collection("bookings")
-    console.log(`Date: ${date}, Service: ${service}, Time: ${time}`)
+    console.log(`Date: ${date}, Service: ${serviceKey}, Time: ${time}`)
 
     // Delete the booking
-    const result = await collection.deleteOne({ date, service, time })
+    const result = await collection.deleteOne({ date, service: serviceKey, time })
 
     if (result.deletedCount === 1) {
       console.log("Successfully deleted one document.")
-      res.status(200).json({ message: "Successfully deleted one document." })
     } else {
       console.log("No documents matched the query. Deleted 0 documents.")
-      res.status(404).json({ message: "No documents matched the query. Deleted 0 documents." })
     }
   } catch (err) {
     console.log(err.stack)
-    res.status(500).json({ message: "An error occurred.", error: err })
+    throw err
   } finally {
     client.close()
   }
 }
+
+export default deleteBooking
